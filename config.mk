982cc79af509761ee92a1513aaa56d16fb5e904f
andr68rus <ashtmb@gmail.com>
2020-03-17 18:32:27 +0300

    chiron-fy

--- config.mk
+++ config.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),sagit))
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),chiron))
 
 VENDOR_PATH := vendor/apps/MiuiCamera
 
